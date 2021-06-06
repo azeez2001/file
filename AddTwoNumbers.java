46a307fccc6884f92629cc5bca8e7870d7580e8d
azeez2001 <84721981+azeez2001@users.noreply.github.com>
2021-06-06 22:39:19 +0530

    2 program

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+	int num1 = 50 , num2 = 150, sum ;
+	sum = num1 + num2 ;
+	System.out.println("Addition of two number is :"+sum);
+
+	}
+
+}
