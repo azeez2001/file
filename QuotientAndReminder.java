c4e65e68e4ed56fbf0213daa4e347baef4f45adf
azeez2001 <84721981+azeez2001@users.noreply.github.com>
2021-06-06 22:40:08 +0530

    3rd program

---
+++ QuotientAndReminder.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class QuotientAndReminder {
+
+	public static void main(String[] args) {
+		int num1 =150, num2 = 20;
+		int quotient = num1 /num2;
+		int remainder = num1 % num2;
+		System.out.println("Quotient is:"+ quotient);
+		System.out.println("Reminder is:"+ remainder);
+	}
+
+}
